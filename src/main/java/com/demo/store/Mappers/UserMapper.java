3057531ac5e1e2cc9a555b1aa534b62a81e2ebd0
lalith <lalithnimmagadda@gmail.com>
2025-06-21 23:35:49 +0530

    added UserDto

---
+++ src/main/java/com/demo/store/Mappers/UserMapper.java
@@ -0,0 +1,15 @@
+package com.demo.store.Mappers;
+
+import com.demo.store.DTOs.UserDto;
+import com.demo.store.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel="spring")
+public interface UserMapper {
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "email", source = "email")
+    UserDto toDto(User user);
+}
+

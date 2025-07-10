2c5121f5b9fc7d4579b0a732f96712947b32f8ee
lalith <lalithnimmagadda@gmail.com>
2025-07-10 18:44:24 +0530

    Generating JWT tokens

--- src/main/java/com/demo/store/controller/AuthController.java
+++ src/main/java/com/demo/store/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.demo.store.controller;
 
+import com.demo.store.DTOs.JwtResponse;
 import com.demo.store.DTOs.LoginRequest;
+import com.demo.store.service.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,16 +18,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody @Valid LoginRequest loginRequest) {
+    public ResponseEntity<JwtResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getEmail(),
                         loginRequest.getPassword()
                 )
         );
-        return ResponseEntity.ok().build();
+        String token= jwtService.generateToken(loginRequest.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler(BadCredentialsException.class)

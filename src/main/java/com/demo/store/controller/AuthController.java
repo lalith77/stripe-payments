8a246f0b5c7fc52980e320f906b1f7d676945fae
lalith <lalithnimmagadda@gmail.com>
2025-07-13 01:50:50 +0530

    Added a controller method to fetch user details from token

--- src/main/java/com/demo/store/controller/AuthController.java
+++ src/main/java/com/demo/store/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.demo.store.controller;
 
 import com.demo.store.DTOs.JwtResponse;
 import com.demo.store.DTOs.LoginRequest;
+import com.demo.store.DTOs.UserDto;
+import com.demo.store.Mappers.UserMapper;
+import com.demo.store.repositories.UserRepository;
 import com.demo.store.service.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
@@ -36,6 +42,24 @@ public class AuthController {
     public boolean validate(@RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            // security context is empty, user is not authenticated
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        String email = auth.getPrincipal().toString();
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            // user not found
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
